68722044bc7f7a8b0121bd5a31a9688f4da35e25
Saumyajeet-Varma <157611771+Saumyajeet-Varma@users.noreply.github.com>
2024-06-19 18:54:29 +0530

    Creating Responsive Navbar

--- HomePage/script.js
+++ HomePage/script.js
@@ -24,5 +24,9 @@ $(document).ready(function () {
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
+        $('.dropdown-content').removeClass('onscreen');
+    });
+     $('.dropdown').click(function () {
+        $('.dropdown-content').toggleClass('onscreen');
     });
 });
